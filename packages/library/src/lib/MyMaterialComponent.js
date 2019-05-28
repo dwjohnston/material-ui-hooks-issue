e7671a9f792984fe3c51706f92149f4cbf05dd84
David Johnston <david@blacksheepcode.com>
2019-05-28 15:11:45 +1000

    Add style hook, that breaks everything.

--- packages/library/src/lib/MyMaterialComponent.js
+++ packages/library/src/lib/MyMaterialComponent.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import Card from "@material-ui/core/Card";
-// const useStyles = makeStyles(theme => ({
-//     root: {
+const useStyles = makeStyles(theme => ({
+    root: {
 
-//     },
-// }));
+    },
+}));
 
 export function MyMaterialComponent({ }) {
-    // const classes = useStyles();
+     const classes = useStyles();   //Does't matter if we don't use it. 
 
     return (<Card>
         This is some component
